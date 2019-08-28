be30c59dd8f408ff40022746ae06319cd99229a7
KSagay <50987038+KSagay@users.noreply.github.com>
2019-08-28 21:51:23 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+function animateHamburger() {
+  $('#hamburger-button').toggleClass('open');
+  /*animation from burger to X*/
+}
+
+function slideMenu(){
+  $('.navbar').toggleClass('open');
+  /*animation for slide down menu*/
+}
+$('#hamburger-button').click(function(){
+  console.log('trying');
+  animateHamburger();
+  slideMenu(); /*attaching click handler to the burger button*/
+});
+$('#fullpage').fullpage();
+FastClick.attach(document.body);
+
+
